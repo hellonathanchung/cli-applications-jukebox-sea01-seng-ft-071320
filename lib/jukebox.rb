300f98b1bdf5ba5bacc3737a189fa273bf080da5
Nathan Chung <chung.nathanh@gmail.com>
2020-07-08 23:04:31 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,23 @@
-# Add your code here
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
+
+
+def help
+  input = gets.strip
+  if input.downcase = "help"
+    puts "I accept the following commands:\n 
+    - help : displays this help message\n 
+    - list : displays a list of songs you can play\n 
+    - play : lets you choose a song to play\n 
+    - exit exits this program"
+  end 
+end 
