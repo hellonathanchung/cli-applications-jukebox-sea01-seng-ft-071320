8c8acb10e02931086226d188739e080afb0558f4
Nathan Chung <chung.nathanh@gmail.com>
2020-07-08 23:36:00 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -28,7 +28,7 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   input = gets.chomp 
-  if input.to_i >= 1 && input.to_i <= song.length 
+  if input.to_i >= 1 && input.to_i <= songs.length 
     puts "Playing #{songs[input.to_i-1]}"
   end 
 end 
